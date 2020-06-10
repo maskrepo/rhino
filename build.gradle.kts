44264fb8749f3a3586cefcdfdc726f852586a56b
AldoLaClasse <alain.meynier@gmail.com>
2020-06-10 14:05:00 +0200

    1er commit assez global du microservice avec un health check

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+val quarkusVersion: String = "1.5.0.Final"
+
 plugins {
     kotlin("jvm") version "1.3.61"
+    id ("io.quarkus") version "1.5.0.Final"
+    id ("org.jetbrains.kotlin.plugin.allopen") version "1.3.72"
 }
 
 group = "fr.convergence.proddoc"
@@ -11,6 +15,12 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation(enforcedPlatform("io.quarkus:quarkus-bom:$quarkusVersion"))
+    implementation("io.quarkus:quarkus-resteasy-jackson")
+    implementation("io.quarkus:quarkus-rest-client")
+    implementation("io.quarkus:quarkus-kotlin")
+    implementation("io.quarkus:quarkus-config-yaml")
+    testImplementation("io.quarkus:quarkus-junit5")
 }
 
 tasks {
