e1090eb539fe5c8f3902076ca06feab9b396af06
AldoLaClasse <alain.meynier@gmail.com>
2020-06-18 16:55:59 +0200

    bricolage

--- src/main/kotlin/fr/convergence/proddoc/Producteur.kt
+++ src/main/kotlin/fr/convergence/proddoc/Producteur.kt
@@ -1,17 +1,12 @@
 package fr.convergence.proddoc
 
-import org.eclipse.microprofile.reactive.messaging.Incoming
 import org.eclipse.microprofile.reactive.messaging.Outgoing
 import javax.enterprise.context.ApplicationScoped
-import javax.ws.rs.GET
-import javax.ws.rs.Path
 
-@Path("/emission")
 @ApplicationScoped
 class Producteur {
 
-/*    @Outgoing("mygreffeout")
-    @GET
-    fun Emission() = "hello from alain"
-*/
+//    @Outgoing("mygreffeout")
+    fun Emission() = ("Alain is the boss")
+
 }
