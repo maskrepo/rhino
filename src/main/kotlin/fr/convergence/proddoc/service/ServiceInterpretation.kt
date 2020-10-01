75753f6bb250942271c57050922b851010fbbc4f
dlocatelli <dimitri.locatelli@docaposte.fr>
2020-10-01 17:32:27 +0200

    appel demande_kbis

--- src/main/kotlin/fr/convergence/proddoc/service/ServiceInterpretation.kt
+++ src/main/kotlin/fr/convergence/proddoc/service/ServiceInterpretation.kt
@@ -1,9 +1,18 @@
 package fr.convergence.proddoc.service
 
+import fr.convergence.proddoc.model.lib.obj.MaskEntete
+import fr.convergence.proddoc.model.lib.obj.MaskMessage
 import fr.convergence.proddoc.model.metier.ClefAccesAuxLots
+import fr.convergence.proddoc.model.metier.KbisDemande
 import fr.convergence.proddoc.model.metier.MaskProduit
 import io.vertx.core.logging.Logger
 import io.vertx.core.logging.LoggerFactory
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToJsonElement
+import org.eclipse.microprofile.reactive.messaging.Channel
+import org.eclipse.microprofile.reactive.messaging.Emitter
+import java.time.LocalDateTime
+import java.util.*
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
 
@@ -26,12 +35,12 @@ class ServiceInterpretation {
         //TODO voir comment controler que l'utilisateur n'envoie pas N fois les même demandes
         if (maskLot.peutOnDemarrerInterpretation == true) throw IllegalStateException("Reception d'une demande redondante d'interpretation de Lot")
         maskLot.peutOnDemarrerInterpretation = true
-        for ( maskProduit in maskLot.produits!!) {
+        for (maskProduit in maskLot.produits!!) {
             when {
                 //TODO ajouter VISU_KBIS et voir le cas ou on à null dans la listeIndicateur
-                maskProduit.evenement.codeProduit == "RCS_KBIS"  -> {
+                maskProduit.evenement.codeProduit == "RCS_KBIS" -> {
                     // TODO pour chaque destinataire
-                    actionDemandeKbis(clefAccesAuxLots,maskProduit)
+                    actionDemandeKbis(clefAccesAuxLots, maskProduit)
                 };
                 else -> throw IllegalStateException("L'evenement n'a pas pu être interpreté correctement")
             }
@@ -41,9 +50,22 @@ class ServiceInterpretation {
         }
     }
 
-fun actionDemandeKbis(clefAccesAuxLots: ClefAccesAuxLots,maskProduit: MaskProduit){
-    ServiceInterpretation.LOG.info("GO actionDemandeKbis : ${clefAccesAuxLots} ")
-}
+    @Inject
+    @field: Channel("kbis_demande")
+    val actionDemandeKbisEmitter: Emitter<MaskMessage>? = null
+
+    fun actionDemandeKbis(clefAccesAuxLots: ClefAccesAuxLots, maskProduit: MaskProduit) {
+        ServiceInterpretation.LOG.info("actionDemandeKbis : ${clefAccesAuxLots} ")
+        val numeroGestion = maskProduit.evenement.mapObjetMetier!!.getValue("REGISTRE")
+            ?: throw IllegalStateException("tentative de déclancher une actionDemandeDeKbis avec parametre non renseigne")
+        val avecApostille = maskProduit.evenement.pourApostille ?: false
+        val kbisDemande = KbisDemande(numeroGestion = numeroGestion, avecApostille = avecApostille, avecSceau = false, avecSignature = false)
+        val maskEntete = MaskEntete(idUnique = UUID.randomUUID().toString(),idLot = clefAccesAuxLots.idLot, idEmetteur = clefAccesAuxLots.idEmetteur, idGreffe = clefAccesAuxLots.idGreffe,typeDemande = "DEMANDE_KBIS",dateHeureDemande = LocalDateTime.now() )
+        val maskMessage = MaskMessage(maskEntete, Json.encodeToJsonElement(kbisDemande), null)
+
+        actionDemandeKbisEmitter?.send(maskMessage)
+
+    }
 
 }
 
